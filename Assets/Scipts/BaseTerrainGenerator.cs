5d005517fdba05aec8a5c33fab6b1406dbc8cd81
hayden-donnelly <donnellyhd@outlook.com>
2023-01-26 17:25:31 -0500

    Work on refactoring diffusion generator.

---
+++ Assets/Scipts/BaseTerrainGenerator.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.Barracuda;
+
+public class BaseTerrainGenerator : MonoBehaviour
+{
+    [SerializeField] protected NNModel modelAsset;
+    protected Model runtimeModel;
+
+    protected TensorMathHelper tensorMathHelper = new TensorMathHelper();
+
+    [SerializeField] protected int modelOutputWidth = 256;
+    [SerializeField] protected int modelOutputHeight = 256;
+    protected int modelOutputArea;
+    protected int channels = 1;
+
+    [SerializeField] Terrain terrain;
+    [SerializeField] protected float heightMultiplier = 10.0f;
+
+    protected delegate Tensor WorkerExecuter(IWorker worker, params object[] args);
+
+    protected virtual void Start()
+    {
+        modelOutputArea = modelOutputWidth * modelOutputHeight;
+        runtimeModel = ModelLoader.Load(modelAsset);
+    }
+
+    protected void SetTerrainHeights(Single[] heightmap)
+    {
+        float[,] newHeightmap = new float[modelOutputWidth, modelOutputHeight];
+        for(int i = 0; i < modelOutputArea; i++)
+        {
+            int x = (int)(i % modelOutputWidth);
+            int y = (int)Math.Floor((double)(i / modelOutputWidth));
+            newHeightmap[x, y] = (float)heightmap[i] * heightMultiplier;
+        }
+
+        terrain.terrainData.SetHeights(0, 0, newHeightmap);
+    }
+
+    protected Single[] GenerateHeightmap(Model model, WorkerExecuter workerExecuter, params object[] args)
+    {
+        // Using ComputePrecompiled worker type for most efficient computation on GPU.
+        // Reference: https://docs.unity3d.com/Packages/com.unity.barracuda@1.0/manual/Worker.html
+        var worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
+
+        Tensor output = workerExecuter(worker, args);
+        Single[] outputArray = output.ToReadOnlyArray();
+
+        output.Dispose();
+        worker.Dispose();
+
+        return outputArray;
+    }
+    
+    protected virtual Tensor DefaultWorkerExecuter(IWorker worker, params object[] args)
+    {
+        return new Tensor(1, modelOutputWidth, modelOutputHeight, 1);
+    }
+}
---
+++ Assets/Scipts/TensorMathHelper.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+using System;
+
+public class TensorMathHelper
+{
+    public Tensor RandomNormalTensor(int batchSize, int width, int height, int channels)
+    {
+        Tensor tensor = new Tensor(batchSize, width, height, channels);
+        System.Random random = new System.Random();
+        for(int i = 0; i < width * height; i++)
+        {
+            // Box-Muller transform.
+            // Reference: https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+            double mean = 0.0f;
+            double stdDev = 1.0f;
+            double u1 = 1.0 - random.NextDouble();
+            double u2 = 1.0 - random.NextDouble();
+            double randomStdNormal = Math.Sqrt(-2.0f * Math.Log(u1)) * Math.Sin(2.0f * Math.PI * u2);
+            double randomNormal = mean + stdDev * randomStdNormal;
+            tensor[i] = (float)randomNormal;
+        }
+        return tensor;
+    }
+
+    public Tensor AddTensor(Tensor tensor1, Tensor tensor2)
+    {
+        if(tensor1.length != tensor2.length)
+        {
+            Debug.LogError("Tensors must be the same size.");
+            return null;
+        }
+
+        Tensor newTensor = new Tensor(tensor1.batch, tensor1.width, tensor1.height, tensor1.channels);
+        for(int i = 0; i < tensor1.length; i++)
+        {
+            newTensor[i] = tensor1[i] + tensor2[i];
+        }
+        return newTensor;
+    }
+
+    public Tensor SubtractTensor(Tensor tensor1, Tensor tensor2)
+    {
+        if(tensor1.length != tensor2.length)
+        {
+            Debug.LogError("Tensors must be the same size.");
+            return null;
+        }
+
+        Tensor newTensor = new Tensor(tensor1.batch, tensor1.width, tensor1.height, tensor1.channels);
+        for(int i = 0; i < tensor1.length; i++)
+        {
+            newTensor[i] = tensor1[i] + tensor2[i];
+        }
+        return newTensor;
+    }
+
+    public Tensor ScaleTensor(Tensor tensor, float scalar)
+    {
+        Tensor newTensor = new Tensor(tensor.batch, tensor.width, tensor.height, tensor.channels);
+        for(int i = 0; i < tensor.length; i++)
+        {
+            newTensor[i] = scalar * tensor[i];
+        }
+        return newTensor;
+    }
+
+    public Tensor RaiseTensorToPower(Tensor tensor, int power)
+    {
+        Tensor newTensor = new Tensor(tensor.batch, tensor.width, tensor.height, tensor.channels);
+        for(int i = 0; i < tensor.length; i++)
+        {
+            newTensor[i] = Mathf.Pow(tensor[i], power);
+        }
+        return newTensor;
+    }
+}
