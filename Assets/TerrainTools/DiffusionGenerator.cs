9ff795062e5b2a16a122bacba8ee801fd2d666c7
hayden-donnelly <hayden.git@proton.me>
2023-03-10 19:50:33 -0500

    Rudimentary generated terrain painting.

--- Assets/TerrainTools/DiffusionGenerator.cs
+++ Assets/TerrainTools/DiffusionGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Unity.Barracuda;
 using UnityEditor;
 using UnityEditor.TerrainTools;
+using UnityEngine.TerrainTools;
 
 public class DiffusionGenerator : TerrainPaintTool<DiffusionGenerator>
 {
@@ -42,6 +43,14 @@ public class DiffusionGenerator : TerrainPaintTool<DiffusionGenerator>
     };
     private UpSampleResolution upSampleResolution = UpSampleResolution._512;
 
+    // Brushes.
+    private bool brushesEnabled = false;
+    private Texture2D brushTexture1;
+    private Texture2D brushTexture2;
+    private float m_BrushOpacity = 0.1f;
+    private float m_BrushSize = 25f;
+    private float m_BrushRotation = 0f;
+
     public override string GetName()
     {
         return "NTG/" + "Diffusion Generator";
@@ -54,6 +63,38 @@ public class DiffusionGenerator : TerrainPaintTool<DiffusionGenerator>
 
     public override void OnInspectorGUI(Terrain terrain, IOnInspectorGUI editContext)
     {
+        if(!brushesEnabled)
+        {
+            if(GUILayout.Button("Enable Brushes"))
+            {
+                brushesEnabled = true;
+            }
+        }
+        else
+        {
+            if(GUILayout.Button("Disable Brushes"))
+            {
+                brushesEnabled = false;
+            }
+            editContext.ShowBrushesGUI(5, BrushGUIEditFlags.Select);
+            m_BrushOpacity = EditorGUILayout.Slider("Opacity", m_BrushOpacity, 0, 1);
+            m_BrushSize = EditorGUILayout.Slider("Size", m_BrushSize, .001f, 1000f);
+            m_BrushRotation = EditorGUILayout.Slider("Rotation", m_BrushRotation, 0, 360);
+            if(GUILayout.Button("Generate Brush Heighmap"))
+            {
+                float[] brushHeightmap = GenerateHeightmap();
+                Color[] colorBrushHeightmap = new Color[brushHeightmap.Length];
+                for(int i = 0; i < brushHeightmap.Length; i++)
+                {
+                    colorBrushHeightmap[i] = new Color(brushHeightmap[i], brushHeightmap[i], brushHeightmap[i]);
+                }
+                brushTexture1 = new Texture2D(upSampledWidth, upSampledHeight);
+                brushTexture1.SetPixels(0, 0, upSampledWidth, upSampledHeight, colorBrushHeightmap);
+                brushTexture1.Apply();
+            }
+            GUILayout.Box(brushTexture1);
+        }
+
         modelAsset = (NNModel)EditorGUILayout.ObjectField("Model Asset", modelAsset, typeof(NNModel), false);
         modelOutputWidth = EditorGUILayout.IntField("Model Output Width", modelOutputWidth);
         modelOutputHeight = EditorGUILayout.IntField("Model Output Height", modelOutputHeight);
@@ -77,6 +118,71 @@ public class DiffusionGenerator : TerrainPaintTool<DiffusionGenerator>
         {
             BlendAllNeighbors(terrain);
         }
+    }
+
+    private void RenderIntoPaintContext(PaintContext paintContext, Texture brushTexture, BrushTransform brushXform)
+    {
+        // Get the built-in painting Material reference
+        Material mat = TerrainPaintUtility.GetBuiltinPaintMaterial();
+        // Bind the current brush texture
+        mat.SetTexture("_BrushTex", brushTexture);
+        // Bind the tool-specific shader properties
+        var opacity = Event.current.control ? -m_BrushOpacity : m_BrushOpacity;
+        mat.SetVector("_BrushParams", new Vector4(opacity, 0.0f, 0.0f, 0.0f));
+        // Setup the material for reading from/writing into the PaintContext texture data. This is a necessary step to setup the correct shader properties for appropriately transforming UVs and sampling textures within the shader
+        TerrainPaintUtility.SetupTerrainToolMaterialProperties(paintContext, brushXform, mat);
+        // Render into the PaintContext's destinationRenderTexture using the built-in painting Material - the id for the Raise/Lower pass is 0.
+        Graphics.Blit(paintContext.sourceRenderTexture, paintContext.destinationRenderTexture, mat, 0);
+    }
+
+    // Render Tool previews in the SceneView
+    public override void OnRenderBrushPreview(Terrain terrain, IOnSceneGUI editContext)
+    {
+        // Dont render preview if this isnt a Repaint
+        if (Event.current.type != EventType.Repaint) return;
+
+        // Only do the rest if user mouse hits valid terrain
+        if (!editContext.hitValidTerrain) return;
+
+        // Get the current BrushTransform under the mouse position relative to the Terrain
+        BrushTransform brushXform = TerrainPaintUtility.CalculateBrushTransform(terrain, editContext.raycastHit.textureCoord, m_BrushSize, m_BrushRotation);
+        // Get the PaintContext for the current BrushTransform. This has a sourceRenderTexture from which to read existing Terrain texture data.
+        PaintContext paintContext = TerrainPaintUtility.BeginPaintHeightmap(terrain, brushXform.GetBrushXYBounds(), 1);
+        // Get the built-in Material for rendering Brush Previews
+        Material previewMaterial = TerrainPaintUtilityEditor.GetDefaultBrushPreviewMaterial();
+        // Render the brush preview for the sourceRenderTexture. This will show up as a projected brush mesh rendered on top of the Terrain
+        TerrainPaintUtilityEditor.DrawBrushPreview(paintContext, TerrainBrushPreviewMode.SourceRenderTexture, brushTexture1, brushXform, previewMaterial, 0);
+        // Render changes into the PaintContext destinationRenderTexture
+        RenderIntoPaintContext(paintContext, brushTexture1, brushXform);
+        // Restore old render target.
+        RenderTexture.active = paintContext.oldRenderTexture;
+        // Bind the sourceRenderTexture to the preview Material. This is used to compute deltas in height
+        previewMaterial.SetTexture("_HeightmapOrig", paintContext.sourceRenderTexture);
+        // Render a procedural mesh displaying the delta/displacement in height from the source Terrain texture data. When modifying Terrain height, this shows how much the next paint operation will alter the Terrain height
+        TerrainPaintUtilityEditor.DrawBrushPreview(paintContext, TerrainBrushPreviewMode.DestinationRenderTexture, brushTexture1, brushXform, previewMaterial, 1);
+        // Cleanup resources
+        TerrainPaintUtility.ReleaseContextResources(paintContext);
+    }
+
+    // Perform painting operations that modify the Terrain texture data
+    public override bool OnPaint(Terrain terrain, IOnPaint editContext)
+    {
+        if(brushesEnabled)
+        {
+            // Get the current BrushTransform under the mouse position relative to the Terrain
+            BrushTransform brushXform = TerrainPaintUtility.CalculateBrushTransform(terrain, editContext.uv, m_BrushSize, m_BrushRotation);
+            // Get the PaintContext for the current BrushTransform. This has a sourceRenderTexture from which to read existing Terrain texture data
+            // and a destinationRenderTexture into which to write new Terrain texture data
+            PaintContext paintContext = TerrainPaintUtility.BeginPaintHeightmap(terrain, brushXform.GetBrushXYBounds());
+            // Call the common rendering function used by OnRenderBrushPreview and OnPaint
+            RenderIntoPaintContext(paintContext, brushTexture1, brushXform);
+            // Commit the modified PaintContext with a provided string for tracking Undo operations. This function handles Undo and resource cleanup for you
+            TerrainPaintUtility.EndPaintHeightmap(paintContext, "Terrain Paint - Raise or Lower Height");
+
+            // Return whether or not Trees and Details should be hidden while painting with this Terrain Tool
+            return true;
+        }
+        return false;
     }
 
     private void CalculateUpSampledDimensions()
@@ -543,16 +649,6 @@ public class DiffusionGenerator : TerrainPaintTool<DiffusionGenerator>
                 upSampled[0, y * factor, x, 0] = p1;
             }
         }
-
-        /*for(int x = 0; x < upSampled.width; x++)
-        {
-            upSampled[0, 0, x, 0] = upSampled[0, 3, x, 0];
-            upSampled[0, 1, x, 0] = upSampled[0, 3, x, 0];
-            upSampled[0, upSampled.height - 1, x, 0] = upSampled[0, upSampled.height - 5, x, 0];
-            upSampled[0, upSampled.height - 2, x, 0] = upSampled[0, upSampled.height - 5, x, 0];
-            upSampled[0, upSampled.height - 3, x, 0] = upSampled[0, upSampled.height - 5, x, 0];
-            upSampled[0, upSampled.height - 4, x, 0] = upSampled[0, upSampled.height - 5, x, 0];
-        }*/
 
         float[] upSampledArray = upSampled.ToReadOnlyArray();
         upSampledX.Dispose();
